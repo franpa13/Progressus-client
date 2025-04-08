7aea6623ac8d331af2544e0394208d1027f61dad
Francisco Javier Paredes <frandelregue@gmail.com>
2025-04-08 12:28:18 -0300

    changed url api

--- src/service/api.js
+++ src/service/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const API_URL = import.meta.env.VITE_API_URL;
-const API_URL = "https://localhost:7140/"
+const API_URL = import.meta.env.VITE_API_URL;
+// const API_URL = "https://localhost:7140/"
 console.log(API_URL, "apiulr");
 
 const api = axios.create({
